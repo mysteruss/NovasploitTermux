b8638489e502ec1286b351625d9cf3344ddbb9f6
NOVASPLOİT <154706019+mysteruss@users.noreply.github.com>
2024-07-03 23:14:02 +0400

    Add files via upload

---
+++ novainstaller.py
@@ -0,0 +1,41 @@
+import os
+
+print ("""┌──────────────────────────────────────────────┐
+│┏━   ┏━┃  ┃ ┃  ┏━┃  ┏━┛  ┏━┃  ┃    ┏━┃  ┛  ━┏┛│
+│┃ ┃  ┃ ┃  ┃ ┃  ┏━┃  ━━┃  ┏━┛  ┃    ┃ ┃  ┃   ┃ │
+│┛ ┛  ━━┛   ┛   ┛ ┛  ━━┛  ┛    ━━┛  ━━┛  ┛   ┛ │
+└──────────────────────────────────────────────┘
+İNSTALLİNG AİRCRACK-NG
+İNSTALLİNG REAVER
+İNSTALLİNG SET
+İNSTALLİNG MALTEGO
+İNSTALLİNG ZAPROXY
+İNSTALLİNG NİKTO
+İNSTALLİNG SHERLOCK
+İNSTALLİNG BURPSUİTE
+İNSTALLİNG HYDRA
+İNSTALLİNG MAXPHİSHER
+İNSTALLİNG SHELL-FİNDER
+İNSTALLİNG GOLİSMERO
+İNSTALLİNG METASPLOİT
+İNSTALLİNG SQLMAP
+İNSTALLİNG NMAP
+""")
+os.system("sudo apt install aircrack-ng")
+os.system("sudo apt install reaver")
+os.system("sudo apt install set")
+os.system("sudo apt install maltego")
+os.system("sudo apt install zaproxy")
+os.system("sudo apt install nikto")
+os.system("sudo apt install sherlock")
+os.system("sudo apt install burpsuite")
+os.system("sudo apt install hydra")
+os.system("pip install maxphisher")
+os.system("git clone https://github.com/AdliXSec/Shell-Finder")
+os.system("git clone https://github.com/golismero/golismero")
+os.system("sudo apt install metasploit")
+os.system("sudo apt install sqlmap")
+os.system("sudo apt install nmap")
+
+
+
