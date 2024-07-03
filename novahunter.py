8eccb6eb697a18068d489d8865c95a95090584b4
NOVASPLOİT <154706019+mysteruss@users.noreply.github.com>
2024-07-03 23:14:46 +0400

    Update novahunter.py

--- novahunter.py
+++ novahunter.py
@@ -12,4 +12,4 @@ print ("""
 os.system("pkg install wget -y")
 os.system("wget -O install-nethunter-termux https://offs.ec/2MceZWr")
 os.system("chmod +x install-nethunter-termux")
-os.system("./install-nethunter-termux")
+os.system("bash install-nethunter-termux")
